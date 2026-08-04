2b2c06c0672fa92309fcb9635ac874e96187e8a3
Zyntro <zyntro.ai.studio@gmail.com>
2026-08-04 23:07:01 +0700

    Create components/navbar.jsx
    Create components/navbar.jsx

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Zyntro App</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Dashboard</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
